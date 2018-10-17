4dcacfa95b2652a8fb9c31a16879a31b90c67789
Bhavesh Kalra <bhavesh.kalra92@gmail.com>
2018-10-17 15:53:40 +0530

    Mail user with subject

--- mailUser.py
+++ mailUser.py
@@ -6,6 +6,7 @@ class Receiver():
     def __init__(self,content,remail):
         self.body=content
         self.remail=remail
+        self.subject="Favourite TV Series Details"
 
     @property
     def get_REmail(self):
@@ -23,12 +24,13 @@ class Receiver():
 
     def mailIt(self):
         try:
-            server=smtplib.SMTP('smtp.gmail.com:587') 
+            server=smtplib.SMTP(config.smtp) 
             server.ehlo() #ehlo is for esmtp server i.e extended smtp server           
             server.starttls()
             server.login(config.username,config.pwd)
-            server.sendmail(config.username,self.remail,self.body)
+            message='Subject: {}\n\n{}'.format(self.subject,self.body)
+            server.sendmail(config.username,self.remail,message)
             server.quit()
-            print("Email successfully sent")
+            print("Email successfully sent!!!\n\n")
         except Exception as error:
             print("Email failed to sent due to:",error)
