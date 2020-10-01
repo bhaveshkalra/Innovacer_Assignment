34393fcbaf5a49cbd35c093f21bbeacd0cc1cad2
Aayush Jain <aj.rt.9597@gmail.com>
2020-10-01 18:32:16 +0530

    Update mailUser.py
    created server request function

--- mailUser.py
+++ mailUser.py
@@ -21,16 +21,21 @@ class Receiver():
 
     def set_Message(self,value):
         self.body=value
+        
+    def serverRequest(self):
+        server=smtplib.SMTP(config.smtp) 
+        server.ehlo() #ehlo is for esmtp server i.e extended smtp server           
+        server.starttls()
+        server.login(config.username,config.pwd)
+        message='Subject: {}\n\n{}'.format(self.subject,self.body)
+        server.sendmail(config.username,self.remail,message)
+        server.quit()
+        print("Email sent successfully!")
 
     def mailIt(self):
         try:
-            server=smtplib.SMTP(config.smtp) 
-            server.ehlo() #ehlo is for esmtp server i.e extended smtp server           
-            server.starttls()
-            server.login(config.username,config.pwd)
-            message='Subject: {}\n\n{}'.format(self.subject,self.body)
-            server.sendmail(config.username,self.remail,message)
-            server.quit()
-            print("Email successfully sent!!!\n\n")
+            serverRequest()
         except Exception as error:
             print("Email failed to sent due to:",error)
+            
+  
